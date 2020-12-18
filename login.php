174e09efc92e8816ba07f48c3d66e3b4ce1bece5
DH51703779 <perseusnguyen210@gmail.com>
2020-12-18 22:36:28 +0700

    them hello va Script

--- login.php
+++ login.php
@@ -153,6 +153,35 @@
                 <span class="text--notify"><?php echo $uid!==0?"Đăng ký thành công! Hãy đăng nhập bằng tài khoản của bạn.":"";?></span>
             </form>
         </div>
+        <div class="overlay-container">
+            <div class="overlay">
+                <div class="overlay-panel overlay-left">
+                    <h1>Welcome Back!</h1>
+                    <p>To keep connected with us please login with your personal info</p>
+                    <button class="ghost" id="signIn">Sign In</button>
+                </div>
+                <div class="overlay-panel overlay-right">
+                    <h1>Hello!</h1>
+                    <p>Enter your personal details and start shopping with us</p>
+                    <button class="ghost" id="signUp">Sign Up</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script>
+        const signUpButton = document.getElementById('signUp');
+        const signInButton = document.getElementById('signIn');
+        const container = document.getElementById('container');
+        signUpButton.addEventListener('click', () => {
+            container.classList.add("right-panel-active");
+        });
+
+        signInButton.addEventListener('click', () => {
+            container.classList.remove("right-panel-active");
+        });
+        
+    </script>
 </body>
 
 </html>
