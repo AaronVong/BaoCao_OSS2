86229dec813337745548964c7a3486bd64d854df
DH51703779 <perseusnguyen210@gmail.com>
2020-12-18 22:30:35 +0700

    them rang buoc va dieu kien cho trang dang nhap

--- login.php
+++ login.php
@@ -11,7 +11,98 @@
 </head>
 
 <body>
-    <h1>Trang đăng trong quá trình thi công...</h1>
+    <?php
+ ob_start();
+ if(!isset($_SESSION)){session_start();}
+ require_once "back-end/DbServices.php";
+ require "back-end/Customer_class.php";
+ $_customer= new Customer();
+ $services = new DbServices();
+     if(!isset($_SESSION)){
+         session_start();
+     }
+ 
+     $name=$email=$password=$repassword=$address=$phone="";
+     $errorExists=false;
+     $uid=0;
+     $signuperror = [];
+     $signinerror=[];
+     if(isset($_POST["signin"])){
+         $email = $_POST["email"];
+         $password = $_POST["password"];
+         
+         if(!$services->checkEmail($email)){
+             $signinerror["email"]="Email không hợp lệ!";
+             $errorExists=true;
+         }
+
+         if(strlen($password)===0 || strlen($password)<8){
+             $signinerror["password"]="Mật khẩu phái có tối thiểu 8 ký tự!";
+             $errorExists=true;
+         }
+
+         if(!$errorExists){
+             $success = $_customer->signIn($email,$password);
+             if($success==1||$success=='1'){
+                 header("Location: index.php");
+             }
+             else{
+                 $signinerror["signin"]="Mật khẩu hoặc email không đúng!";
+             }
+         }
+     }
+
+     if(isset($_POST["signup"])){
+         $name=$_POST["name"];
+         $email = $_POST["email"];
+         $password = $_POST["password"];
+         $repassword = $_POST["repassword"];
+         $phone = $_POST["phone"];
+         $address = $_POST["address"];
+
+         if(!$services->checkRegularString($name)){
+             $signuperror["name"]="Tên phải có ít nhất 5 ký tự và không được chứa ký tự đặc biệt và số!";
+             $errorExists=true;
+         }
+
+         if(!$services->checkEmail($email)){
+             $signuperror["email"]="Email không hợp lệ!";
+             $errorExists=true;
+         }else{
+             if($_customer->isEmailExist($email)){
+                 $signuperror["email"]="Email đã được đăng ký!";
+                 $errorExists=true;
+             }
+         }
+
+         if(strlen($password)===0 || strlen($password)<8){
+             $signuperror["password"]="Mật khẩu phái có tối thiểu 8 ký tự!";
+             $errorExists=true;
+         }
+
+         if($repassword!==$password){
+             $signuperror["repassword"]="Mật khẩu không trùng khớp!";
+             $errorExists=true;
+         }
+
+         if(!$services->checkAddress($address)){
+             $signuperror["address"]="Địa chỉ không được để trống và không được chứa ký tự đặc biệt!";
+             $errorExists=true;
+         }
+
+         if(!$services->checkPhone($phone)){
+             $signuperror["phone"]="Số điện thoại không hợp lệ!";
+             $errorExists=true;
+         }
+
+         if(!$errorExists){
+             $uid=$_customer->signUp($name, $email,$address,$phone,$password);
+             if(!$uid){
+                  $signuperror["signup"]="Đăng ký thất bại, hãy thử lại!";
+             }
+         }
+     }
+    ?>
 </body>
 
 </html>
